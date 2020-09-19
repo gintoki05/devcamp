f595bfd753fe1fcd870ca2ef98c73c428219167e
gintoki <ajieprastyo@gmail.com>
2020-09-19 20:33:38 +0700

    add aggregation

---
+++ routes/bootcamps.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
+// Include other ressource routers
+const courseRouter = require('./courses');
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/').get(getBootcamps).post(createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
